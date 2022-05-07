11ff1f35c2a5afcfc3ec9afc051012892f30b0d5
AliQTank <104526578+AliQTank@users.noreply.github.com>
2022-05-07 13:33:35 -0500

    Update chromeExtTutorial.js

--- chromeExtTutorial.js
+++ chromeExtTutorial.js
@@ -1,6 +1,10 @@
+let myLeads = [];
+const inputBtn = document.getElementById("input-btn")
+const inputEl = document.getAnimations("input-element")
 
-let inputBtn = document.getElementById("input-btn")
+//Push the value "www.awesomeleads.com" to myArray when the input is clicked
 
 inputBtn.addEventListener("click", function(){
-    alert("Button clicked from addEventListener")
+    myLeads.push("www.awesomeleads.com")
+    console.log(myLeads)
 })
