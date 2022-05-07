aef5c5a0f644f808244121a7bfff403d6ad0f901
AliQTank <104526578+AliQTank@users.noreply.github.com>
2022-05-07 14:21:41 -0500

    Update chromeExtTutorial.js

--- chromeExtTutorial.js
+++ chromeExtTutorial.js
@@ -1,6 +1,9 @@
 let myLeads = ["w", "r", "3"];
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-element")
+const ulEl = document.getElementById("unordered-list")
+console.log(ulEl)
+// 2.- Grab the unordered list and store it in a const variable called ulEl
 
 inputBtn.addEventListener("click", function(){    
     let inputing = inputEl.value
@@ -8,8 +11,9 @@ inputBtn.addEventListener("click", function(){
     console.log(myLeads)
 })
 
-//Log out the items in the myLeads array using a for loop
 for(let i = 0; i < myLeads.length; i++){
     console.log(myLeads[i])       
 }
+
+
 
