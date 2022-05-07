6a8d2b73c86e0ab7c07b5d9e14b8ffce67f7a990
AliQTank <104526578+AliQTank@users.noreply.github.com>
2022-05-07 15:57:55 -0500

    Update chromeExtTutorial.js

--- chromeExtTutorial.js
+++ chromeExtTutorial.js
@@ -1,25 +1,23 @@
-let myLeads = ["facebook", "youtube", "stackoverflow"];
+let myLeads = [];
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-element")
 const ulEl = document.getElementById("unordered-list")
 
 inputBtn.addEventListener("click", function(){    
     let inputing = inputEl.value
-    myLeads.push(inputing)   
-    console.log(myLeads)
-    
-})
+    myLeads.push(inputing)       
+    //2. Call the renderLeads() function
+    renderLeads()
+ })
 
-//Create a variable, listItems, to hold all the HTML for the list items
-//Assign it to an empty string to begin with
-let listItems = ""
-for(let i = 0; i < myLeads.length; i++){   
+//1. Wrap the code below in a renderLeads() function
+function renderLeads(){
+    let listItems = ""
+    for(let i = 0; i < myLeads.length; i++){   
     //2. Add the item to the listItems variable instead of the ulEl.innerHTML 
-    
     listItems += "<li>" + myLeads[i] + "</li>"
 }
-
-//3. Render the listItems inside the unordered list using ulEl.innerHTML
-
 ulEl.innerHTML = listItems
+
+}
 
