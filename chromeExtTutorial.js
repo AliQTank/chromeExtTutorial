4d4a78da4ed0d2eb598791098d3cc1d2fbd4207d
AliQTank <104526578+AliQTank@users.noreply.github.com>
2022-05-07 15:51:30 -0500

    Update chromeExtTutorial.js

--- chromeExtTutorial.js
+++ chromeExtTutorial.js
@@ -7,13 +7,19 @@ inputBtn.addEventListener("click", function(){
     let inputing = inputEl.value
     myLeads.push(inputing)   
     console.log(myLeads)
+    
 })
 
-//Different method
-for(let i = 0; i < myLeads.length; i++){    
-    const li = document.createElement("li") // used to create Li¿I tag
-    li.textContent = myLeads[i] //used to ADD content to LI tag
-    ulEl.append(li) //to put Li into de UL id"unordered-list""
+//Create a variable, listItems, to hold all the HTML for the list items
+//Assign it to an empty string to begin with
+let listItems = ""
+for(let i = 0; i < myLeads.length; i++){   
+    //2. Add the item to the listItems variable instead of the ulEl.innerHTML 
+    
+    listItems += "<li>" + myLeads[i] + "</li>"
 }
 
+//3. Render the listItems inside the unordered list using ulEl.innerHTML
+
+ulEl.innerHTML = listItems
 
